1787aef52d702d76dd8e5692ea8087dd642a7a85
bararchy <bar.hofesh@safe-t.com>
2015-05-27 18:42:40 +0300

    Name Changes

--- spec/crystal-libinjection_spec.cr
+++ spec/crystal-libinjection_spec.cr
@@ -2,19 +2,41 @@ require "./spec_helper"
 
 describe "Libinjection" do
   it "works" do
-    puts "Scanning 'Hello', is there an SQL injection ?"
-    test = Libinjection::SqliScan.string("Hello")
-    if test == 0
-      puts "No Injection".colorize.green
-    elsif test == 1
-      puts "Injection".colorize.red
+
+    puts "Scanning 'Hello', should return 'String is clear' ?"
+    if Libinjection::SqliScan.contains_sqli?("Hello")
+      puts "SQL injection found".colorize.green
+    else
+      puts "String is clear".colorize.green
     end
-    puts "Scanning 'Hello' OR 'a'='a' --DROP TABLE', is there an SQL injection ?"
-    test = Libinjection::SqliScan.string("Hello' OR 'a'='a' --DROP TABLE")
-    if test == 0
-      puts "No Injection".colorize.red
-    elsif test == 1
-      puts "Injection".colorize.green
+
+    puts "Scanning 'Hello' OR 'a'='a' --DROP TABLE', should return 'SQL injection found' ?"
+    if Libinjection::SqliScan.contains_sqli?("Hello' OR 'a'='a' --DROP TABLE")
+      puts "SQL injection found".colorize.green
+    else
+      puts "String is clear".colorize.green
+    end
+
+    puts "Scanning nil data, this should raise NilData"
+    begin
+      if Libinjection::SqliScan.contains_sqli?(nil)
+        puts "SQL injection found".colorize.red
+      else
+        puts "String is clear".colorize.red
+      end
+    rescue e
+      puts e.to_s.colorize.green
+    end
+
+    puts "Scanning empty data, this should raise EmptyData"
+    begin
+      if Libinjection::SqliScan.contains_sqli?("")
+        puts "SQL injection found".colorize.red
+      else
+        puts "String is clear".colorize.red
+      end
+    rescue e
+      puts e.to_s.colorize.green
     end
   end
 end
--- src/crystal-libinjection.cr
+++ src/crystal-libinjection.cr
@@ -4,12 +4,23 @@ module Libinjection
 
   class SqliScan
 
-    def self.string(data)
-      raise "NilString" if data.nil?
-      raise "EmptyString" if data.empty?
-      sfilter :: Lib_injection::SFilter
-      Lib_injection.libinjection_init(pointerof(sfilter), data, data.bytesize, 0)
-      Lib_injection.libinjection_is_sqli?(pointerof(sfilter))
+    def self.contains_sqli?(data)
+      raise "NilData" if data.nil?
+      if data
+        raise "EmptyData" if data.empty?
+        sfilter :: Lib_injection::SFilter
+        Lib_injection.libinjection_init(pointerof(sfilter), data, data.bytesize, 0)
+        result = Lib_injection.libinjection_is_sqli?(pointerof(sfilter))
+        if result == 0
+          return false
+        elsif result == 1
+          return true
+        else
+          return false
+        end
+      else
+        return false
+      end
     end
   end
 end
